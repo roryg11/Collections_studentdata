dbc049c8e672741eb63c8a8eab1e9f5f82bb8951
roryg11 <Rory.C.Grant@gmail.com>
2014-10-31 17:07:05 -0700

    student data with no syntax error

--- student_info.rb
+++ student_info.rb
@@ -1,46 +1,47 @@
-student_info = [
-{
+student_info =
+[
+  {
     firstname: "John",
     lastname: "Foley",
     email: "john@gschool.it",
     class: "Beginning Snark",
-  }
+  },
   {
     firstname: "Kelsey",
     lastname: "Sylwester",
     email: "sellie@gmail.com",
     class: "Ruby Immersive"
-  }
+  },
   {
     firstname: "Timothy",
     lastname: "Rama",
-    email:"Tim.Rama@gmail.com",
+    email: "Tim.Rama@gmail.com",
     class: "Ruby Immersive",
-  }
+  },
   {
     firstname:"Kane",
     lastname:"Baccigalupi",
     email:"Kane@gschool.it",
     class:"C for dummies"
-  }
+  },
   {
     firstname: "Nikita",
     lastname:"Theodosius",
     email:"Nikita.Theo@gmail.com",
     class: "Ruby Immersive"
-  }
+  },
   {
     firstname: "Roddy",
     lastname: "Eldred",
     email: "roddy.el@gmail.com",
     class: "Ruby Immersive"
-  }
+  },
   {
     firstname: "Martha",
     lastname: "Berner",
     email: "Martha@gschool.it",
     class: "Time Travel for Dummies"
-  }
+  },
   {
     firstname: "Kofi",
     lastname:"Thomas",
